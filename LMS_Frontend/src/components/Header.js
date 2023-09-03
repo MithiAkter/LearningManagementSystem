053353d3c3d6378e9f29b65bc9335e8e17029de7
mithikona <mithilakona599@gmail.com>
2023-09-04 01:42:40 +0600

    Created initial design for Dashboard, Sidebar/Menubar, MyCourses, FavoriteCourses, Recommendedcourses, & Profilesetting

--- LMS_Frontend/src/components/Header.js
+++ LMS_Frontend/src/components/Header.js
@@ -4,21 +4,34 @@ function Header() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
                     <Link className="navbar-brand" to="/">Programming Learning System</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav ms-auto">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        <a className="nav-link" href="#">Courses</a>
-                        <a className="nav-link" href="#">Teachers</a>
-                        <Link className="nav-link" to="/user-login">User Login</Link>
-                        <Link className="nav-link" to="/user-register">User Register</Link>
-                        
-                    </div>
+                        <div className="navbar-nav ms-auto">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            {/* <a className="nav-link" href="#">Courses</a>
+                            <a className="nav-link" href="#">Teachers</a> */}
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" 
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                    User
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><Link className="dropdown-item" to="/user-login">Login</Link></li>
+                                    <li><Link className="dropdown-item" to="/user-register">Register</Link></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><Link className="dropdown-item" to="/user-dashboard">Dashboard</Link></li>
+                                    <li><a className="dropdown-item" href="#">Logout</a></li>
+                                </ul>
+                            </li>
+                            <a className="nav-link" href="#">Courses</a>
+                            <a className="nav-link" href="#">Teachers</a> 
+                        </div>
                     </div>
                 </div>
-        </nav>
+            </nav>
         
     );
   }
--- LMS_Frontend/src/components/Main.js
+++ LMS_Frontend/src/components/Main.js
@@ -1,10 +1,17 @@
 import Header from './Header';
 import Home from './Home';
 import CourseDetail from "./CourseDetail";
-import About from './About';
-import Footer from './Footer';
 import Login from './User/Login';
 import Register from './User/Register';
+import Dashboard from './User/Dashboard';
+import MyCourses from './User/MyCourses';
+import FavoriteCourses from './User/FavoriteCourses';
+import RecommendedCourses from './User/RecommendedCourses';
+import ProfileSetting from './User/ProfileSetting';
+import ChangePassword from './User/ChangePassword';
+import About from './About';
+import Footer from './Footer';
+
 
 import {Routes as Switch, Route} from 'react-router-dom';
 
@@ -19,6 +26,12 @@ function Main() {
             <Route path='/detail/:course_id' element={<CourseDetail />} />
             <Route path='/user-login' element={<Login />} />
             <Route path='/user-register' element={<Register />} />
+            <Route path='/user-dashboard' element={<Dashboard />} />
+            <Route path='/my-courses' element={<MyCourses />} />
+            <Route path='/favorite-courses' element={<FavoriteCourses />} />
+            <Route path='/recommended-courses' element={<RecommendedCourses />} />
+            <Route path='/profile-setting' element={<ProfileSetting />} />
+            <Route path='/change-password' element={<ChangePassword />} />
         </Switch>
       <Footer/>
     </div>
