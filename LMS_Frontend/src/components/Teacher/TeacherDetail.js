ec8ce9c10a103000e3bc269e63bff046e9a0fc6f
mithikona <mithilakona599@gmail.com>
2023-09-07 23:16:48 +0600

    teacherDetails added

---
+++ LMS_Frontend/src/components/Teacher/TeacherDetail.js
@@ -0,0 +1,11 @@
+import {useParams} from "react-router-dom";
+import {Link} from 'react-router-dom';
+
+function TeacherDetail(){
+    
+    return(
+        <h3>This is teacher detail page</h3>
+        );
+    }
+    export default TeacherDetail;
+
