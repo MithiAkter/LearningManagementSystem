049a7037c6daca550c421c03dc9ee6922631f1dd
mithikona <mithilakona599@gmail.com>
2023-09-08 20:15:19 +0600

    added watch video feature  in CourseDetail

--- LMS_Frontend/src/components/CourseDetail.js
+++ LMS_Frontend/src/components/CourseDetail.js
@@ -32,8 +32,27 @@ function CourseDetail(){
                         <li className="list-group-item d-flex justify-content-between align-items-center">Introduction
                         <span className="float-end">
                             <span className="me-5">1 Hour 30 Minutes</span>
-                            <button className="btn btn-sm btn-danger"><i class="bi bi-youtube"></i></button>
+                            <button className="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#videoModal1"><i class="bi bi-youtube"></i></button>
                         </span> 
+
+                            {/* Video Modal Starts */}
+                            <div className="modal fade" id="videoModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-xl">
+                                <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLabel">Video 1</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <div class="ratio ratio-16x9">
+                                    <iframe src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" title="YouTube video" allowfullscreen></iframe>
+                                    </div>
+                                </div>
+                                
+                                </div>
+                            </div>
+                            </div>
+                            {/* Video Modal Ends  */}
                         </li>
                         <li className="list-group-item d-flex justify-content-between align-items-center">Introduction
                         <span className="float-end">
